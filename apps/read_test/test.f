d44bc5e7a0091c72e5051145763bf22842843ed4
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-06-11 13:33:29 +0300

    improved the input files

--- apps/read_test/test.f
+++ apps/read_test/test.f
@@ -3,22 +3,16 @@
       
       REAL, DIMENSION(30):: DATA_1
       REAL, DIMENSION(30):: DATA_2
-      INTEGER :: DIM_1=5,DIM_2=5
+      INTEGER :: DIM_1=17,DIM_2=18
       INTEGER :: I
       INTEGER :: FD_1=20,FD_2=21
 
       PRINT *,"Reader Program"
 
-      OPEN(FD_1,FILE='BA120P.dat',STATUS='OLD')
+      OPEN(FD_1,FILE='BA120P.DAT',STATUS='OLD')
 
       ! Reading the data from first file -> BA120P.DAT
-      !DO I=1,DIM_1
-       ! WRITE(FD_1,*) I,SIN(I+0.1)
-      !END DO
-
-      !CLOSE(FD_1)
-      
-      OPEN(FD_1,FILE='BA120P.dat',STATUS='OLD')
+      OPEN(FD_1,FILE='BA120P.DAT',STATUS='OLD')
       
       DO I=1,DIM_1
         READ(FD_1,*) DATA_1(I)
