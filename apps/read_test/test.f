ac300d9a5f83aea795a6fae7d86401fcccc94333
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-06-11 13:38:47 +0300

    clean the input for the first file

--- apps/read_test/test.f
+++ apps/read_test/test.f
@@ -1,24 +1,31 @@
       PROGRAM READER
       IMPLICIT NONE
       
-      REAL, DIMENSION(30):: DATA_1
-      REAL, DIMENSION(30):: DATA_2
+      !Arrays for storing the experimental data from the two input files
+      !Store data from first file and the second file
+      REAL, DIMENSION(30):: DATA_1,DATA_2
+
+      !Size of the two arrays
       INTEGER :: DIM_1=17,DIM_2=18
-      INTEGER :: I
+      
+      !Labels for the file descriptors that are used for opening and closing files: Must be integers
       INTEGER :: FD_1=20,FD_2=21
+      
+      !Iterator
+      INTEGER :: I
 
       PRINT *,"Reader Program"
 
+      !Opening the first data file
       OPEN(FD_1,FILE='BA120P.DAT',STATUS='OLD')
 
       ! Reading the data from first file -> BA120P.DAT
-      OPEN(FD_1,FILE='BA120P.DAT',STATUS='OLD')
-      
       DO I=1,DIM_1
         READ(FD_1,*) DATA_1(I)
       END DO
-
+      !Close the descriptor of the first file
       CLOSE(FD_1)
+
 
       DO I=1,DIM_1
        PRINT *, I, DATA_1(I)
