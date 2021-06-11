01ce47b066a4fa66cafa5e51e3e434c5e11e04cb
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-06-11 11:31:45 +0300

    add math functions and writing data to file

--- apps/read_test/test.f
+++ apps/read_test/test.f
@@ -7,10 +7,20 @@
 
       PRINT *,"Reader Program"
 
-      DO I =1,5
+      DO I =1,17
        DATA_1(I)=1
       END DO
 
-      PRINT *, DATA_1
+      OPEN(1,FILE='DATA1.DAT',STATUS='OLD')
+
+      DO I=1,17
+        WRITE(1,*) DATA_1(I)
+      END DO
+
+      CLOSE(1)
+
+      DO I=1,17
+        PRINT *, I, DATA_1(I), 2*SIN(DATA_1(I))
+      END DO
 
       END PROGRAM READER
