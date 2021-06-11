5c183e81de7064863267242defb0f795fcfea058
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-06-11 11:18:59 +0300

    add simple math test

---
+++ apps/read_test/test.f
@@ -0,0 +1,16 @@
+      PROGRAM READER
+      IMPLICIT NONE
+      
+      REAL :: N1, N2, RESULT
+
+      PRINT *,"Reader Program"
+
+
+      N1=1
+      N2=2
+
+      RESULT=N1+N2
+
+      PRINT *, RESULT
+
+      END PROGRAM READER
